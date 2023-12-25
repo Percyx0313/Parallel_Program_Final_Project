814bfac74a152e3dce428031b57cd06c30a0e85b
Percyx0313 <percyx987654321@gapp.nthu.edu.tw>
2023-12-25 19:04:12 +0800

    The BaseLine Radix Sort

---
+++ radix_sort.cu
@@ -0,0 +1,222 @@
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <math.h>
+#include <cuda_runtime.h>
+#include <fstream>
+using namespace std;
+#define MAX_NUM_LISTS 256
+
+cudaEvent_t start, stop;
+float *Data,*dev_srcData,*dev_dstData;
+
+
+// radix sort
+__global__ void GPU_radix_sort(float* src_data, float*  dest_data, \
+    int num_lists, int num_data);
+__device__ void radix_sort(float*  data_0, float*  data_1, \
+    int num_lists, int num_data, int tid); 
+__device__ void merge_list( float* src_data, float*  dest_list, \
+    int num_lists, int num_data, int tid); 
+__device__ void preprocess_float(float*  data, int num_lists, int num_data, int tid);
+__device__ void Aeprocess_float(float*  data, int num_lists, int num_data, int tid);
+
+// Auxiliariy function
+void INPUT(char* FIN,int N);
+void OUTPUT(char* FOUT,int N);
+void SHOW(int N);
+bool EVALUATE(int N);
+int main(int argc, char **argv){
+    if(argc!=4){
+        cout << "The input format must be ./program N input_data output_path" <<endl;
+    }
+    
+    int N=atoi(argv[1]);
+    char* FIN=argv[2];
+    char* FOUT=argv[3];
+    float   elapsedTime;
+    cudaEventCreate( &start );
+    cudaEventCreate( &stop );
+
+    // input 
+    Data=new float[N];
+    INPUT(FIN,N);
+    SHOW(N);
+
+    // cuda preprocess
+    cudaMalloc((void**)&dev_srcData,sizeof(float)*N);
+    cudaMalloc((void**)&dev_dstData,sizeof(float)*N);
+    cudaMemcpy(dev_srcData, Data, sizeof(float)*N, cudaMemcpyHostToDevice);
+
+    int num_lists = 128; // the number of parallel threads
+    // radix sort
+    cudaEventRecord( start, 0 ) ;
+    GPU_radix_sort<<<1,num_lists>>>(dev_srcData, dev_dstData, num_lists, N);
+    cudaEventRecord( stop, 0 ) ;
+    cudaEventSynchronize( stop );
+
+    // cuda postprocess
+    cudaMemcpy(Data, dev_dstData, sizeof(float)*N, cudaMemcpyDeviceToHost);
+    // output
+    cudaFree(dev_srcData);
+    cudaFree(dev_dstData);
+    OUTPUT(FOUT,N);
+
+    // Test the correstness and output the running time
+    cudaEventElapsedTime( &elapsedTime,start, stop );
+    cout << "The Evaluation STATUS \n";
+    cout << "==================================\n";
+    cout << "CORRECTNESS : "<< (EVALUATE(N)==true?"PASS":"FAIL")<<"\nCOST TIME : "<< elapsedTime << endl;
+    cout << "==================================\n";
+    return 0;
+}
+
+void SHOW(int N){
+    cout << "========== Current Data ==========\n";
+    for(int i=0;i<N;i++){
+        cout << setw(8) <<Data[i]<<" ";
+    }
+    cout << "\n==================================\n";
+}
+
+void INPUT(char* FIN,int N){
+    FILE* file = fopen(FIN, "rb");
+    fread(Data,sizeof(float)*N,N,file);
+    fclose(file);
+}
+
+void OUTPUT(char* FOUT,int N){
+    FILE* file = fopen(FOUT, "w");
+    fwrite(Data,sizeof(float)*N,N,file);
+    fclose(file);
+}
+bool EVALUATE(int N){
+    bool PASS=true;
+    float prev=Data[0];
+    for(int i =0;i<N;i++)
+    {
+        if(prev>Data[i]){
+            PASS=false;
+        }
+        prev=Data[i];
+    }
+    return PASS;
+}
+
+__global__ void GPU_radix_sort(float*  src_data, float*  dest_data, \
+    int num_lists, int num_data) {
+    // temp_data:temporarily store the data
+    int tid = blockIdx.x*blockDim.x + threadIdx.x;
+    // special preprocessing of IEEE floating-point numbers before applying radix sort
+    preprocess_float(src_data, num_lists, num_data, tid); 
+    __syncthreads();
+   // no shared memory
+    radix_sort(src_data, dest_data, num_lists, num_data, tid);
+    __syncthreads();
+    merge_list(src_data, dest_data, num_lists, num_data, tid);    
+    __syncthreads();
+    Aeprocess_float(dest_data, num_lists, num_data, tid);
+    __syncthreads();
+}
+
+__device__ void preprocess_float(float*  src_data, int num_lists, int num_data, int tid)
+{
+    for(int i = tid;i<num_data;i+=num_lists)
+    {
+        unsigned int *data_temp = (unsigned int *)(&src_data[i]);    
+        *data_temp = (*data_temp >> 31 & 0x1)? ~(*data_temp): (*data_temp) | 0x80000000; 
+    }
+}
+
+__device__ void Aeprocess_float(float*  data, int num_lists, int num_data, int tid)
+{
+    for(int i = tid;i<num_data;i+=num_lists)
+    {
+        unsigned int* data_temp = (unsigned int *)(&data[i]);
+        *data_temp = (*data_temp >> 31 & 0x1)? (*data_temp) & 0x7fffffff: ~(*data_temp);
+    }
+}
+
+
+__device__ void radix_sort(float*  data_0, float*  data_1, \
+    int num_lists, int num_data, int tid) 
+{
+    for(int bit=0;bit<32;bit++)
+    {
+        int bit_mask = (1 << bit);
+        int count_0 = 0;
+        int count_1 = 0;
+        for(int i=tid; i<num_data;i+=num_lists)
+        {
+            unsigned int *temp =(unsigned int *) &data_0[i];
+            if(*temp & bit_mask)
+            {
+                data_1[tid+count_1*num_lists] = data_0[i]; //bug 在这里 等于时会做强制类型转化
+                count_1 += 1;
+            }
+            else{
+                data_0[tid+count_0*num_lists] = data_0[i];
+                count_0 += 1;
+            }
+        }
+        for(int j=0;j<count_1;j++)
+        {
+            data_0[tid + count_0*num_lists + j*num_lists] = data_1[tid + j*num_lists]; 
+        }
+    }
+}
+
+__device__ void merge_list( float* src_data, float*  dest_list, \
+    int num_lists, int num_data, int tid) 
+{
+    int num_per_list = ceil((float)num_data/num_lists);
+    __shared__ int list_index[MAX_NUM_LISTS];
+    __shared__ float record_val[MAX_NUM_LISTS];
+    __shared__ int record_tid[MAX_NUM_LISTS];
+    list_index[tid] = 0;
+    record_val[tid] = 0;
+    record_tid[tid] = tid;
+    __syncthreads();
+    for(int i=0;i<num_data;i++)
+    {
+        record_val[tid] = 0;
+        record_tid[tid] = tid; // bug2 每次都要进行初始化
+        if(list_index[tid] < num_per_list)
+        {
+            int src_index = tid + list_index[tid]*num_lists;
+            if(src_index < num_data)
+            {
+                record_val[tid] = src_data[src_index];
+            }else{
+                unsigned int *temp = (unsigned int *)&record_val[tid];
+                *temp = 0xffffffff;
+            }
+        }else{
+                unsigned int *temp = (unsigned int *)&record_val[tid];
+                *temp = 0xffffffff;
+        }
+        __syncthreads();
+        int tid_max = num_lists >> 1;
+        while(tid_max != 0 )
+        {
+            if(tid < tid_max)
+            {
+                unsigned int* temp1 = (unsigned int*)&record_val[tid];
+                unsigned int *temp2 = (unsigned int*)&record_val[tid + tid_max];
+                if(*temp2 < *temp1)
+                {
+                    record_val[tid] = record_val[tid + tid_max];
+                    record_tid[tid] = record_tid[tid + tid_max];
+                }
+            }
+            tid_max = tid_max >> 1;
+            __syncthreads();
+        }
+        if(tid == 0)
+        {
+            list_index[record_tid[0]]++;
+            dest_list[i] = record_val[0];
+        }
+        __syncthreads();
+    }
+}
